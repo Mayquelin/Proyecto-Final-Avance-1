cb599b07e16db2eea62425cb8dfa821d02351a20
Mayquelin <145294597+Mayquelin@users.noreply.github.com>
2025-10-19 14:05:32 -0600

    Create protecto.M.java with main method
    Class main

---
+++ protecto.M.java
@@ -0,0 +1,19 @@
+package Logica;
+
+import Logica.Juego;
+
+public class proyectoM {
+
+public class Proyecto {
+    public static void main(String[] args) {
+        System.out.println("=== The Sandwich Guy ===");
+        
+        //juego o el menú
+        Juego juego = new Juego();
+        juego.iniciar(); //estos seria para despues compañeros
+        
+        System.out.println("Juego finalizado. ¡Gracias por jugar!");
+    }
+}
+
+    }
